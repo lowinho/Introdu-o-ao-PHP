5efeaf6175e320185c9f316dce0560d8a288b9c4
Cesar Lowe <lowecesarlowe@gmail.com>
2020-09-09 13:11:19 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+$categorias = [];
+$categorias[] = 'Infantil';
+$categorias[] = 'Adolescente';
+$categorias[] = 'Adulto';
+
+print_r($categorias);
+
+$nome = 'Eduardo';
+$Idade = 18;
+
+var_dump($nome);
+var_dump($idade);
+
+if($idade >= 6 && $idade <= 12) {
+  for($i = 0; $i <= count($categorias); $i++) {
+    if($categorias[$i] == 'infantil')
+
+    echo "O nadador " .$nome. " compete na categoria infantil"
+  }
+} else if ($idade >= 13 && $idade <= 18) {
+
+  for($i = 0; $i <= count($categorias); $i++) {
+    if($categorias[$i] == 'adolescente')
+
+    echo "O nadador " .$nome. " compete na categoria infantil" //ponto concatena string
+  }
+
+} else {
+  for($i = 0; $i <= count($categorias); $i++) {
+    if($categorias[$i] == 'adulto')
+
+    echo "O nadador " .$nome. " compete na categoria infantil"
+  }
+}
